ac9f52bf7107ef66792783e373a1c2393dcd129a
nicgowans <43365329+nicgowans@users.noreply.github.com>
2020-08-06 15:10:17 +1000

    Update xyz.py
    Update xyz.py for use with common_fn.py and pynadjust_classes.py

--- pynadjust/xyz.py
+++ pynadjust/xyz.py
@@ -1,155 +1,175 @@
 # pynadjust xyz module - retrieve data from .xyz files
+
 import geodepy.convert as gc
-import datetime
+from pynadjust_classes import Station, DynaMetadata
+import common_fn
 
 
-class StationLibrary(object):
-    def __init__(self):
-        self.stations = {}
-        self.epoch = None
-        self.reference_frame = None
-        self.geoid_model = None
-        self.version = None
-        self.file_name = None
-        self.file_date = None
+class DynaXYZ(object):
+    def __init__(self, filename):
+        """
+        DynaXYZ object which reads all metadata and coordinate information upon initialisation
+        :param filename: name of xyz file to be consumed.
+        """
 
-    class Station(object):
-        def __init__(self):
-            self.name = None
-            self.con = None
-            self.lat = None
-            self.lon = None
-            self.ohgt = None
-            self.ehgt = None
-            self.sd_e = None
-            self.sd_n = None
-            self.sd_u = None
+        def read_xyz(xyz_file):
+            """
+            function/method to read metadata and coordinate information from a DynAdjust station file.
+            :param xyz_file: DynAdjust *.xyz file
+            :return: dictionary of stn objects, and metadata variables
+            """
+            stns = {}
+            version = None
+            reference_frame = None
+            file_name = None
+            file_date = None
+            epoch = None
+            geoid_model = None
 
-        def xyz(self):
-            return gc.llh2xyz(self.lat, self.lon, self.ehgt)
+            with open(xyz_file, 'r') as xyz_fh:
+                line_count = 0
+                stn_line = False
+                stn_switch = False
+                metadata_switch = True
+                mandatory_coord_types = 'PLHh'
+                desc_index = None
 
-        def grid(self):
-            return gc.geo2grid(self.lat, self.lon)
+                for line_count, line in enumerate(xyz_fh):
+                    # line_count += 1
+
+                    if 'Adjusted Coordinates' in line:
+                        stn_line = line_count + 5
+
+                    if stn_line:
+                        if line_count == stn_line - 2:
+                            desc_index = line.find('Description')
+
+                        if line_count == stn_line:
+                            stn_switch = True
+                            metadata_switch = False
+
+                            metadata = DynaMetadata(reference_frame=reference_frame, epoch=epoch,
+                                                    geoid_model=geoid_model, version=version)
+
+                    if metadata_switch:
+                        version = common_fn.read_metadata(line, 'Version:', version)
+                        reference_frame = common_fn.read_metadata(line, 'Reference frame:', reference_frame)
+                        file_name = common_fn.read_metadata(line, 'File name:', file_name)
+                        file_date = common_fn.read_file_date(line, file_date)
+                        epoch = common_fn.read_epoch(line, epoch)
+                        geoid_model = common_fn.read_metadata(line, 'Geoid model:', geoid_model)
+
+                        if line[:35] == 'Station coordinate types:          ':
+                            coord_types = line[35:].strip()
+                            missing_coord_types = set()
+                            for l in mandatory_coord_types:
+                                if l not in coord_types:
+                                    missing_coord_types.add(l)
+                            if missing_coord_types:
+                                raise ValueError(f'Mandatory coordinate types {missing_coord_types} not present in {xyz_file}')
+
+                    if stn_switch:
+                        if len(line) < 20:
+                            stn_switch = False
+                            continue
+
+                        stn_object = read_coord_elements(line, coord_types, desc_index)
+
+                        stns[stn_object.name] = stn_object
+
+            return stns, file_name, file_date, metadata
+
+        results = read_xyz(filename)
+        self.stations = results[0]
+        self.file_name = results[1]
+        self.file_date = results[2]
+        self.metadata = results[3]
 
 
-def read_xyz(xyz_file):
+def read_coord_elements(line, coord_types, desc_index):
+    """
+    Function to read coordinate components from a line in an adj/xyz file
+    :param line: coordinate line
+    :param coord_types: string of coordinate types on line
+    :param desc_index: position of station descriptions on line
+    :return: Station object
+    """
 
-    dyna_xyz = StationLibrary()
+    lat = None
+    lon = None
+    ohgt = None
+    ehgt = None
 
-    with open(xyz_file, 'r') as xyz_fh:
-        line_count = 0
-        stn_line = False
-        stn_switch = False
-        mandatory_coord_types = 'PLHh'
+    stn = line[0:20].strip()
+    con = line[20:23]
+    results = line[25:].split()
+    r_count = 0
 
-        for line in xyz_fh:
-            line_count += 1
+    for ct in coord_types:
+        if ct == 'P':
+            lat = gc.hp2dms(float(results[r_count]))
+        if ct == 'L':
+            lon = gc.hp2dms(float(results[r_count]))
+        if ct == 'H':
+            ohgt = float(results[r_count])
+        if ct == 'h':
+            ehgt = float(results[r_count])
 
-            if 'Adjusted Coordinates' in line:
-                stn_line = line_count + 5
+        r_count += 1
 
-            if stn_line:
-                if line_count == stn_line:
-                    stn_switch = True
+    # Don't forget about the qualities
+    sd_e = float(results[r_count])
+    sd_n = float(results[r_count + 1])
+    sd_u = float(results[r_count + 2])
 
-            if line[:35] == 'Version:                           ':
-                dyna_xyz.version = line[35:].strip()
+    # Station Description
+    if desc_index is None or desc_index == -1:
+        desc = ''
+    else:
+        desc = str(line[desc_index:].strip())
 
-            if line[:35] == 'Reference frame:                   ':
-                dyna_xyz.reference_frame = line[35:].strip()
+    # store details as a Station object, leaving uncertainty fields null
+    stn_object = Station(
+        name=stn,
+        con=con,
+        lat=lat,
+        lon=lon,
+        ehgt=ehgt,
+        ohgt=ohgt,
+        sd_e=sd_e,
+        sd_n=sd_n,
+        sd_u=sd_u,
+        description=desc,
+        hpu=None,
+        vpu=None,
+        smaj=None,
+        smin=None,
+        brg=None,
+        vcv=None,
+        covariances={}
+    )
 
-            if line[:35] == 'File name:                         ':
-                dyna_xyz.file_name = line[35:].strip()
-
-            if line[:35] == 'File created:                      ':
-                date_str = line[35:].strip()
-                dyna_xyz.file_date = datetime.datetime.strptime(date_str, '%A, %d %B %Y, %I:%M:%S %p')
-                dyna_xyz.file_name = line[35:].strip()
-
-            if line[:35] == 'Epoch:                             ':
-                date_str = line[35:].strip()
-                d = int(date_str[:2])
-                m = int(date_str[3:5])
-                y = int(date_str[6:])
-                dyna_xyz.epoch = datetime.date(y, m, d)
-
-            if line[:35] == 'Geoid model:                       ':
-                dyna_xyz.geoid_model = line[35:].strip()
-
-            if line[:35] == 'Station coordinate types:          ':
-                coord_types = line[35:].strip()
-                missing_coord_types = set()
-                for l in mandatory_coord_types:
-                    if l not in coord_types:
-                        missing_coord_types.add(l)
-                if missing_coord_types:
-                    raise ValueError(f'Mandatory coordinate types {missing_coord_types} not present in {xyz_file}')
-
-            if stn_switch:
-                if len(line) < 20:
-                    stn_switch = False
-                    continue
-
-                P = None
-                L = None
-                H = None
-                h = None
-
-                stn = line[0:20].strip()
-                con = line[20:23]
-                results = line[25:].split()
-                r_count = 0
-
-                for ct in coord_types:
-                    if ct == 'P':
-                        P = gc.hp2dec(float(results[r_count]))
-                    if ct == 'L':
-                        L = gc.hp2dec(float(results[r_count]))
-                    if ct == 'H':
-                        H = float(results[r_count])
-                    if ct == 'h':
-                        h = float(results[r_count])
-
-                    r_count += 1
-
-                # Don't forget about the qualities
-                sd_e = float(results[r_count])
-                sd_n = float(results[r_count + 1])
-                sd_u = float(results[r_count + 2])
-
-                # initialise a Station object in dictionary, then populate fields
-                dyna_xyz.stations[stn] = dyna_xyz.Station()
-                dyna_xyz.stations[stn].name = stn
-                dyna_xyz.stations[stn].con = con
-                dyna_xyz.stations[stn].lat = P
-                dyna_xyz.stations[stn].lon = L
-                dyna_xyz.stations[stn].ehgt = h
-                dyna_xyz.stations[stn].ohgt = H
-                dyna_xyz.stations[stn].sd_e = sd_e
-                dyna_xyz.stations[stn].sd_n = sd_n
-                dyna_xyz.stations[stn].sd_u = sd_u
-
-    return dyna_xyz
+    return stn_object
 
 
 # ----------------------------------------------------------------------
 # Example of usage
 # ----------------------------------------------------------------------
 
-xyz_file = ''
+xyz_file = '300236_GDA2020-MC.simult.xyz'
 
 if xyz_file:
-    xyz_results = read_xyz(xyz_file)
+    xyz_results = DynaXYZ(xyz_file)
 
     # iterate through and print coordinates
     out_str = ''
     # print metadata
     out_str += f'File name:       {xyz_results.file_name}\n'
-    out_str += f'Version:         {xyz_results.version}\n'
     out_str += f'Date created:    {xyz_results.file_date}\n'
-    out_str += f'Reference Frame: {xyz_results.reference_frame}\n'
-    out_str += f'Epoch:           {xyz_results.epoch}\n'
-    out_str += f'Geoid model:     {xyz_results.geoid_model}\n'
+    out_str += f'Version:         {xyz_results.metadata.version}\n'
+    out_str += f'Reference Frame: {xyz_results.metadata.reference_frame}\n'
+    out_str += f'Epoch:           {xyz_results.metadata.epoch}\n'
+    out_str += f'Geoid model:     {xyz_results.metadata.geoid_model}\n'
 
     # print results
     out_str += '\nStations:\n'
@@ -158,8 +178,8 @@ if xyz_file:
         out_str += '{:20s} {:3s} {:14.8f} {:14.8f} {:10.4f} {:10.4f} {:8.4f} {:8.4f} {:8.4f} '.format(
             xyz_results.stations[stn].name,
             xyz_results.stations[stn].con,
-            xyz_results.stations[stn].lat,
-            xyz_results.stations[stn].lon,
+            xyz_results.stations[stn].lat.dec(),
+            xyz_results.stations[stn].lon.dec(),
             xyz_results.stations[stn].ehgt,
             xyz_results.stations[stn].ohgt,
             xyz_results.stations[stn].sd_e,
